8e8743f8aa979734b6dc67cb2c07f6369f4d88ce
DMV Throwers Yoyo Club <dmvthrowers@gmail.com>
2026-04-23 17:20:37 -0400

    favicon

--- yoyomap/app/layout.tsx
+++ yoyomap/app/layout.tsx
@@ -128,6 +128,9 @@ const Footer = () => (
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className="min-h-screen flex flex-col">
         <TopBar />
         <header className="sticky top-0 z-40">
