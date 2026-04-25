2cd34390caf21e1e24d4759c97631e783e7a726a
DMV Throwers Yoyo Club <dmvthrowers@gmail.com>
2026-04-25 19:09:46 -0400

    Update page.tsx

--- yoyomap/app/[locale]/legal/security/page.tsx
+++ yoyomap/app/[locale]/legal/security/page.tsx
@@ -7,6 +7,7 @@ import { notFound } from "next/navigation";
 import { marked } from "marked";
 
 // This page renders the April 2026 security incident bulletin as HTML from the Markdown file.
+export default async function SecurityIncidentPage() {
   // Read the markdown file directly from the filesystem
   let mdContent = "";
   try {
